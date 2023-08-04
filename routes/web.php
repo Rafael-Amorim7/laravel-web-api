37d535e7c468351a8fad2cfc89bdad44985cecf7
Rafael Amorim <rafaamorim1504@gmail.com>
2023-08-04 00:13:58 -0300

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SeriesController;
-use App\Http\Controllers\SeasonsController;
-use App\Http\Controllers\EpisodesController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
-    return redirect('/series');
+    return view('welcome');
 });
 
-// https://laravel.com/docs/10.x/controllers#actions-handled-by-resource-controller
-Route::resource('/series', SeriesController::class)
-    ->except(['show']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])
-->name('seasons.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
+require __DIR__.'/auth.php';
