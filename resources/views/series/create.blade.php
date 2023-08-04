29d46e3a3eaa5e2d2b4ae06f0279cdedf7abe7b2
Rafael Amorim <rafaamorim1504@gmail.com>
2023-08-03 21:43:00 -0300

    apply bootstrap series pages

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,16 +1,24 @@
 <x-layout title="Create Serie">
     <form action="{{ route('series.store') }}" method="POST">
     @csrf
-    <label for="name">Name</label>
-    <input type="name" id="name" name="name" value="{{ old('name') }}">
+        <div class="row mb-3">
+            <div class="col-8">
+                <label class="form-label" for="name">Name</label>
+                <input class="form-control" type="name" id="name" name="name" value="{{ old('name') }}">
+            </div>
 
-    <label for="seasons">Seassons</label>
-    <input type="name" id="seasons" name="seasons" value="{{ old('seasons') }}">
+            <div class="col-2">
+                <label class="form-label" for="seasons">Seassons</label>
+                <input class="form-control" type="name" id="seasons" name="seasons" value="{{ old('seasons') }}">
+            </div>
 
-    <label for="episodes">episodes</label>
-    <input type="name" id="episodes" name="episodes" value="{{ old('episodes') }}">
+            <div class="col-2">
+                <label for="episodes" class="form-label">Episodes</label>
+                <input type="name" class="form-control" id="episodes" name="episodes" value="{{ old('episodes') }}">
+            </div>
+        </div>
 
-    <button type="submit">Create</button>
+        <button type="submit" class="btn btn-primary">Create</button>
 </form>
 
 </x-layout>
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,23 +1,29 @@
 <x-layout title="Series">
-    <a href="{{ route('series.create') }}">Create</a>
-    @isset($mensagem)
-    <div class="alert alert-success">
-        {{ $mensagem }}
-    </div>
-    @endisset
-    <ul>
+    <a href="{{ route('series.create') }}" class="btn btn-dark mb-2">Adicionar</a>
+
+    <ul class="list-group">
         @foreach ($series as $serie)
-            <a href="{{ route('series.edit', $serie->id) }}">Edit</a>
-            <a href="{{ route('seasons.index', $serie->id) }}"> {{ $serie->name }} </a>
-            <form method="POST" action="{{ route('series.destroy', $serie->id) }}">
-                @csrf
-                @method('DELETE')
-                <button type="submit">X</button>
-            </form>
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+
+            <a href="{{ route('seasons.index', $serie->id) }}">
+                {{ $serie->name }}
+            </a>
+
+            <span class="d-flex">
+                <a href="{{ route('series.edit', $serie->id) }}" class="btn btn-primary btn-sm">
+                    E
+                </a>
+
+                <form action="{{ route('series.destroy', $serie->id) }}" method="post" class="ms-2">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger btn-sm">
+                        X
+                    </button>
+                </form>
+            </span>
+        </li>
         @endforeach
     </ul>
 
-    <script>
-        const series = {{ Js::from($series) }};
-    </script>
 </x-layout>
