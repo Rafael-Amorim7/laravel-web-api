b5c5219ec7acf47293db62d59cbb6c7e520f6c58
Rafael Amorim <rafaamorim1504@gmail.com>
2023-08-05 13:00:06 -0300

    tests

---
+++ tests/Feature/SeriesRepositoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Repositories\EloquentSeriesRepository;
+use App\Http\Requests\SeriesFormRequest;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SeriesRepositoryTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_when_create_series_should_also_create_its_seasons_and_episodes(): void
+    {
+        $repository = $this->app->make(EloquentSeriesRepository::class);
+        $request = new SeriesFormRequest([
+            'name' => 'Series Name',
+            'seasons' => 1,
+            'episodes' => 1,
+        ]);
+
+        $repository->add($request);
+
+        $this->assertDatabaseHas('series', ['name' => 'Series Name']);
+        $this->assertDatabaseHas('seasons', ['number' => 1]);
+        $this->assertDatabaseHas('episodes', ['number' => 1]);
+    }
+}
