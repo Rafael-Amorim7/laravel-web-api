44099a612d795efdbcd8b171d5ce9273dfad3866
Rafael Amorim <rafaamorim1504@gmail.com>
2023-08-05 17:13:16 -0300

    Accessor

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -2,9 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 
 class Episode extends Model
 {
@@ -22,4 +23,12 @@ class Episode extends Model
     //{
     //    $query->where('watched', true);
     //}
+
+    protected function watched(): Attribute
+    {
+        return new Attribute(
+            get: fn ($watched) => (bool) $watched,
+            set: fn ($watched) => (bool) $watched,
+        );
+    }
 }
