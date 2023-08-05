a9422b85d147492f436a1b0f8d6651f7f4273b57
Rafael Amorim <rafaamorim1504@gmail.com>
2023-08-05 18:26:52 -0300

    middleware

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,13 @@ use App\Http\Controllers\Api\SeasonsController;
 use App\Http\Controllers\Api\ImageUploadController;
 use App\Http\Controllers\Api\EpisodesController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/series', SeriesController::class);
+    Route::get('/series/{series}/episodes', [SeriesController::class, 'episodes']);
+
+    Route::post('/upload', [ImageUploadController::class, 'upload']);
+
+    Route::get('/series/{series}/seasons', [SeasonsController::class, 'index']);
+
+    Route::patch('/episodes/{episode}', [EpisodesController::class, 'watched']);
 });
-
-Route::apiResource('/series', SeriesController::class);
-Route::get('/series/{series}/episodes', [SeriesController::class, 'episodes']);
-
-Route::post('/upload', [ImageUploadController::class, 'upload']);
-
-Route::get('/series/{series}/seasons', [SeasonsController::class, 'index']);
-
-Route::patch('/episodes/{episode}', [EpisodesController::class, 'watched']);
